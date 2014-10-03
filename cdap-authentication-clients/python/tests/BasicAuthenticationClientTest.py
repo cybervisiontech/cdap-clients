a85471c71325227ea93391f7f012e667159789b2
Ali Anwar <ali@cask.co>
2014-10-02 22:05:32 -0700

    I guess we still need this, if we want to be able to run tests from any directory. (or if using testrunners, such as nosetest or py.test

--- cdap-authentication-clients/python/tests/BasicAuthenticationClientTest.py
+++ cdap-authentication-clients/python/tests/BasicAuthenticationClientTest.py
@@ -21,6 +21,12 @@ except ImportError:
 import socket
 import threading
 import os
+import inspect
+
+current_dir = os.path.dirname(
+    os.path.abspath(inspect.getfile(inspect.currentframe())))
+parent_dir = os.path.dirname(current_dir)
+sys.path.insert(0, parent_dir)
 
 from AuthDisabledHandler import AuthDisabledHandler
 from EmptyUrlListHandler import EmptyUrlListHandler
